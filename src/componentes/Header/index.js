cc37ee9e53dfef96733e47f6a5fbd4782a6cdf92
devanaclimgo <anaclimgo@gmail.com>
2024-10-03 16:03:32 -0300

    css substituido por styled-components p1

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -2,14 +2,21 @@ import './estilo.css';
 import Logo from '../../componentes/Logo/index.js'
 import OpcoesHeader from '../../componentes/OpcoesHeader/index.js';
 import IconesHeader from '../../componentes/IconesHeader/index.js';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  background-color: #FFF;
+  display: flex;
+  justify-content: space-evenly;
+`
 
 function Header() {
   return (
-    <header className="App-header">
+    <HeaderContainer>
       <Logo></Logo>
       <OpcoesHeader></OpcoesHeader>
       <IconesHeader></IconesHeader>
-    </header>
+    </HeaderContainer>
   );
 }
 
