d2c8ecba79b5b5ba3f9ef039e4961ac3413d6c10
devanaclimgo <anaclimgo@gmail.com>
2024-10-03 18:10:10 -0300

    ultimos lançamentos section

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,9 @@
+import livro2 from '../../img/livros/Nodejs.svg'
+import livro3 from '../../img/livros/Tuning.svg'
+import livro5 from '../../img/livros/livro.png'
+
+export const livros = [
+  {nome: 'Construindo APIs REST com Node.js', id: 2, src: livro2},
+  {nome: 'Tuning de SQL', id: 3, src: livro3},
+  {nome: 'Liderança em Design', id: 5, src: livro5},
+]
